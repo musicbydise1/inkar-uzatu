28432ccfb5269930b08d0dc4809ac9dbc0de30b7
aksultanyessenbay <aksultane912@gmail.com>
2025-03-07 09:51:52 +0500

    second

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Moon+Dance&display=swap"
+                rel="stylesheet"/>
+        </Head>
+        <body className="antialiased">
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
